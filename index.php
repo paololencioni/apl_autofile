bf23a63ff373b83de46f27ee0b71e128d5720293
paololencioni <lencionipaolo@gmail.com>
2014-12-29 20:37:51 +1000

    Signed-off-by: paololencioni <lencionipaolo@gmail.com>

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <p>
-hello world
+<?php
+$dir    = 'C:\Users\paolo.ATLANTICPACIFIC\Documents\apl_test_records';
+$files1 = scandir($dir);
+$files2 = scandir($dir, 1);
+
+print_r($files1);
+print_r($files2);
+?>
 </p>
